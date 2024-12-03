074ac4def1bb6c91dee0568adc269b905071d976
Ilya Mikhailov <fun4lsd@gmail.com>
2024-12-03 18:13:10 +0400

    404 implemented.

--- main.ts
+++ main.ts
@@ -5,6 +5,11 @@ import { router } from './src/router.ts'
 const app = new Application()
 app.use(router.routes())
 app.use(router.allowedMethods())
+app.use((context) => {
+  context.response.type = 'text/html; charset=utf-8'
+  context.response.status = 404
+  context.response.body = '<h1>404, Page not found!</h1>'
+})
 
 Deno.serve({ port: 1337 }, async (req) => {
   let response = await filter(req)
